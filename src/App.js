ec310f64c0676ecf81df62adeca5645c38f3c996
kwametsunami <kwame.kubi@hotmail.com>
2023-04-29 14:10:23 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import MainPage from './pages/mainPage/MainPage';
+
 
 function App() {
   return (
     <div className="App">
       <h1>oscar anesetti</h1>
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/mainPage/MainPage.js
@@ -0,0 +1,11 @@
+import "./MainPage.scss"
+
+const MainPage = () => {
+    return (
+        <div className="mainPage">
+            <h1 className="name">Oscar Anesetti</h1>
+        </div>
+    )
+}
+
+export default MainPage;
