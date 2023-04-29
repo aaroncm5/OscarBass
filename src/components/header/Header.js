ecdb55d8ed85bd9d939021de06d40f29b4a8e1c3
Aaron Mahabir <aaroncurtis32@gmail.com>
2023-04-29 14:10:28 -0400

    fixing partials

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import instagram from '../../assets/icons/instagram.png';
 import facebook from '../../assets/icons/facebook.png';
 import email from '../../assets/icons/email.png';
 import tiktok from '../../assets/icons/tiktok.png'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -11,12 +12,12 @@ const Header = () => {
           <img src="" alt="Oscars Logo" />
         </a>
       </div>
-      <nav className="navLinks">
-        <ul className="siteLinks">
-          <li>about</li>
-          <li>music</li>
-          <li>videos</li>
-          <li>shows</li>
+      <nav className="nav">
+        <ul className="nav-list">
+          <NavLink className='nav-list__item' to='/'>about</NavLink>
+          <NavLink className='nav-list__item' to='/'>music</NavLink>
+          <NavLink className='nav-list__item' to='/'>videos</NavLink>
+          <NavLink className='nav-list__item' to='/'>shows</NavLink>
         </ul>
         <ul className="socialLinks">
           <li>
